04d25e4ac04259d618bf4e5067d925adf7476d1e
Wessel W. Bakker <wwbakker@gmail.com>
2018-09-09 10:16:07 +0200

    chapter 10 again

--- src/main/scala/nl/wwbakker/catssandbox/chapter10/ParallelFoldMap.scala
+++ src/main/scala/nl/wwbakker/catssandbox/chapter10/ParallelFoldMap.scala
@@ -4,22 +4,18 @@ import cats.Monoid
 import nl.wwbakker.catssandbox.chapter9.FoldMap
 
 import scala.concurrent.Future
-import cats.instances.list._
 import cats.instances.future._
-import cats.syntax.traverse._
+import scala.concurrent.ExecutionContext.Implicits.global
 
 object ParallelFoldMap {
-  implicit val ec = scala.concurrent.ExecutionContext.global
   def parallelFoldMap[A, B : Monoid]
-    (values: Vector[A])
-    (func: A => B): Future[B] = {
+  (values: Vector[A])
+  (func: A => B): Future[B] = {
     val numberOfProcessors = Runtime.getRuntime.availableProcessors()
-    val groupedValues = values.grouped(values.size / numberOfProcessors)
-    val groupedResults : List[Future[B]] =
-      groupedValues.map(valuesPerProcessor => Future(FoldMap.foldMap(valuesPerProcessor)(func))).toList
-    groupedResults.sequence
-
+    val groupedValues = values.grouped(Math.ceil(values.size / numberOfProcessors).toInt)
+    FoldMap.foldMap(groupedValues.toVector)(valuesPerProcessor => Future(FoldMap.foldMap(valuesPerProcessor)(func)))
   }
+
 
   def exampleF(a : Int) : String = {
     Thread.sleep(100)
