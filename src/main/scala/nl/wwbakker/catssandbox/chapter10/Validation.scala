565dfc002b48034eaac906e69e37722d39f4a5f1
Wessel W. Bakker <wwbakker@gmail.com>
2018-09-09 11:58:42 +0200

    Done until 10.4.1

--- src/main/scala/nl/wwbakker/catssandbox/chapter10/Validation.scala
+++ src/main/scala/nl/wwbakker/catssandbox/chapter10/Validation.scala
@@ -3,47 +3,62 @@ package nl.wwbakker.catssandbox.chapter10
 import cats.data.NonEmptyList
 import cats._
 import cats.implicits._
+import cats.data.Validated
+import cats.data.Validated.Valid
 
-case class CheckF[E, A](f : A => Either[E, A]) {
-  def and(other : CheckF[E, A])(implicit s : Semigroup[E]) : CheckF[E, A] =
-    CheckF{ a =>
-      (f(a), other.f(a)) match {
-        case (Left(e1), Left(e2)) => Left(s.combine(e1, e2))
-        case (Left(e), Right(_)) => Left(e)
-        case (Right(_), Left(e)) => Left(e)
-        case (Right(v), _) => Right(v)
-      }
-    }
-}
+//case class CheckF[E, A](f : A => Either[E, A]) {
+//  def and(other : CheckF[E, A])(implicit s : Semigroup[E]) : CheckF[E, A] =
+//    CheckF{ a =>
+//      (f(a), other.f(a)) match {
+//        case (Left(e1), Left(e2)) => Left(s.combine(e1, e2))
+//        case (Left(e), Right(_)) => Left(e)
+//        case (Right(_), Left(e)) => Left(e)
+//        case (Right(v), _) => Right(v)
+//      }
+//    }
+//}
+//
 
 object Check {
-  def apply[E, A](f : A => Either[E, A]) : Check[E, A] = Pure(f)
+  def apply[E, A](f : A => Validated[E, A]) : Check[E, A] = Pure(f)
 }
 
 trait Check[E, A] {
-  def check(value: A)(implicit s : Semigroup[E]): Either[E, A]
+  def check(value: A)(implicit s : Semigroup[E]): Validated[E, A]
 
   def and(other : Check[E, A]) : Check[E, A] =
     And(this, other)
 
 }
-case class Pure[E, A](f : A => Either[E, A]) extends Check[E, A] {
-  override def check(value: A)(implicit s : Semigroup[E]): Either[E, A] = f(value)
+case class Pure[E, A](f : A => Validated[E, A]) extends Check[E, A] {
+  override def check(value: A)(implicit s : Semigroup[E]): Validated[E, A] = f(value)
 }
 case class And[E, A](left : Check[E, A], right : Check[E, A]) extends Check[E, A] {
-  override def check(value: A)(implicit s : Semigroup[E]): Either[E, A] =
-    (left.check(value)(s), right.check(value)(s)) match {
-      case (Left(e1), Left(e2)) => Left(s.combine(e1, e2))
-      case (Left(e), Right(_)) => Left(e)
-      case (Right(_), Left(e)) => Left(e)
-      case (Right(v), _) => Right(v)
+  implicit val takeFirstSemigroup : Semigroup[A] = new Semigroup[A] {
+    override def combine(x: A, y: A): A = x
+  }
+
+  override def check(value: A)(implicit s : Semigroup[E]): Validated[E, A] =
+    left.check(value).combine(right.check(value))
+}
+
+case class Or[E, A](left : Check[E, A], right : Check[E, A]) extends Check[E, A] {
+  implicit val takeFirstSemigroup : Semigroup[A] = new Semigroup[A] {
+    override def combine(x: A, y: A): A = x
+  }
+
+  override def check(value: A)(implicit s: Semigroup[E]): Validated[E, A] =
+    (left.check(value), right.check(value)) match {
+      case (Valid(x), _) => x.valid[E]
+      case (_, Valid(x)) => x.valid[E]
+      case (x, y) => x.combine(y)
     }
 }
 
 object TestCheck {
   type CheckEL[A] = Check[NonEmptyList[String], A]
 
-  type ErrorOr[A] = Either[NonEmptyList[String], A]
+  type ErrorOr[A] = Validated[NonEmptyList[String], A]
 
   val valueNonEmpty : CheckEL[String] = Check[NonEmptyList[String], String](
       _.pure[ErrorOr].ensure(NonEmptyList("value is empty", Nil))(_.nonEmpty))
