120a8a685d2bb839758f128efb2e104eaf6200ef
Wessel W. Bakker <wwbakker@gmail.com>
2018-09-15 15:13:09 +0200

    t/m 11.3

--- src/main/scala/nl/wwbakker/catssandbox/chapter11/A.scala
+++ src/main/scala/nl/wwbakker/catssandbox/chapter11/A.scala
@@ -1,17 +1,19 @@
 package nl.wwbakker.catssandbox.chapter11
 
-final case class GCounter(counters: Map[String, Int]) {
-  def increment(machine: String, amount: Int): GCounter =
-    GCounter(counters.updated(machine, counters.getOrElse(machine, 0) + amount))
+object A {
+  final case class GCounter(counters: Map[String, Int]) {
+    def increment(machine: String, amount: Int): GCounter =
+      GCounter(counters.updated(machine, counters.getOrElse(machine, 0) + amount))
 
 
-  def merge(that: GCounter): GCounter =
-    GCounter(
-      (counters.keySet ++ that.counters.keySet).map { machine =>
-        machine -> Integer.max(counters(machine), that.counters(machine))
-      }.toMap
-    )
+    def merge(that: GCounter): GCounter =
+      GCounter(
+        (counters.keySet ++ that.counters.keySet).map { machine =>
+          machine -> Integer.max(counters(machine), that.counters(machine))
+        }.toMap
+      )
 
-  def total: Int =
-    counters.values.sum
+    def total: Int =
+      counters.values.sum
+  }
 }
--- src/main/scala/nl/wwbakker/catssandbox/chapter11/B.scala
+++ src/main/scala/nl/wwbakker/catssandbox/chapter11/B.scala
@@ -1,17 +1,29 @@
 package nl.wwbakker.catssandbox.chapter11
 
 import cats.Monoid
+import cats.instances.list._   // for Monoid
+import cats.instances.map._    // for Monoid
+import cats.syntax.semigroup._ // for |+|
+import cats.syntax.foldable._  // for combineAll
 
-trait BoundedSemiLattice[A] extends Monoid[A] {
-  def combine(a1: A, a2: A): A
-  def empty: A
-}
+object B {
 
+  trait BoundedSemiLattice[A] extends Monoid[A] {
+    def combine(a1: A, a2: A): A
+    def empty: A
+  }
 
-object BoundedSemiLattice {
-  implicit val Ints: BoundedSemiLattice[Int] =
-    new BoundedSemiLattice[Int] {
+  trait Max[A] extends BoundedSemiLattice[A]
+  trait Sum[A] extends BoundedSemiLattice[A]
+
+  implicit val IntsMerge: Max[Int] =
+    new Max[Int] {
       override def combine(a1: Int, a2: Int): Int = a1 max a2
+      override def empty: Int = 0
+    }
+  implicit val IntsSum: Sum[Int] =
+    new Sum[Int] {
+      override def combine(a1: Int, a2: Int): Int = a1 + a2
       override def empty: Int = 0
     }
 
@@ -21,4 +33,21 @@ object BoundedSemiLattice {
         a1 ++ a2
       override def empty: Set[A] = Set.empty[A]
     }
+
+
+  final case class GCounter[A](counters: Map[String, A]) {
+    def increment(machine: String, amount: A)(implicit m : Sum[A]): GCounter[A] = {
+      val v = counters.getOrElse(machine, m.empty) |+| amount
+      GCounter(counters.updated(machine, v))
+    }
+
+
+
+    def merge(that: GCounter[A])(implicit m : Max[A]): GCounter[A] =
+      GCounter[A](this.counters |+| that.counters)
+
+    def total(implicit m : Sum[A]): A =
+      counters.values.toList.combineAll
+  }
 }
+
