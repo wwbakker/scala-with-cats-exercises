f04faae315bb4114de1ed4f23cb7e42914a03f03
Wessel W. Bakker <wwbakker@gmail.com>
2018-09-15 15:29:43 +0200

    Boek uit

--- src/main/scala/nl/wwbakker/catssandbox/chapter11/B.scala
+++ src/main/scala/nl/wwbakker/catssandbox/chapter11/B.scala
@@ -34,20 +34,49 @@ object B {
       override def empty: Set[A] = Set.empty[A]
     }
 
+  trait GCounter[F[_, _], K, V] {
+    def increment(f: F[K, V])(k: K, v: V)
+                 (implicit m: Sum[V]): F[K, V]
 
-  final case class GCounter[A](counters: Map[String, A]) {
-    def increment(machine: String, amount: A)(implicit m : Sum[A]): GCounter[A] = {
-      val v = counters.getOrElse(machine, m.empty) |+| amount
-      GCounter(counters.updated(machine, v))
+    def merge(f1: F[K, V], f2: F[K, V])
+             (implicit b: BoundedSemiLattice[V]): F[K, V]
+
+    def total(f: F[K, V])
+             (implicit m: Sum[V]): V
+  }
+
+  object GCounter {
+    def apply[F[_,_], K, V]
+    (implicit counter: GCounter[F, K, V]) =
+      counter
+  }
+
+  implicit def MapGCounter[K, V] : GCounter[Map, K, V] = new GCounter[Map, K, V] {
+    override def increment(f: Map[K, V])(k: K, v: V)(implicit m: Sum[V]): Map[K, V] = {
+      val newValue = f.getOrElse(k, m.empty) |+| v
+      f.updated(k, newValue)
     }
 
+    override def merge(f1: Map[K, V], f2: Map[K, V])(implicit b: BoundedSemiLattice[V]): Map[K, V] =
+      f1 |+| f2
 
-
-    def merge(that: GCounter[A])(implicit m : Max[A]): GCounter[A] =
-      GCounter[A](this.counters |+| that.counters)
-
-    def total(implicit m : Sum[A]): A =
-      counters.values.toList.combineAll
+    override def total(f: Map[K, V])(implicit m: Sum[V]): V =
+      f.values.toList.combineAll
   }
+
+//  final case class GCounter[A](counters: Map[String, A]) {
+//    def increment(machine: String, amount: A)(implicit m : Sum[A]): GCounter[A] = {
+//      val v = counters.getOrElse(machine, m.empty) |+| amount
+//      GCounter(counters.updated(machine, v))
+//    }
+//
+//
+//
+//    def merge(that: GCounter[A])(implicit m : Max[A]): GCounter[A] =
+//      GCounter[A](this.counters |+| that.counters)
+//
+//    def total(implicit m : Sum[A]): A =
+//      counters.values.toList.combineAll
+//  }
 }
 
